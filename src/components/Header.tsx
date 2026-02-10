c0d031e77a0eb7bf43254ef5fb11a2af88e01182
AmandaPersdotter <amanda.vivianne.p.l@hotmail.com>
2026-02-10 18:04:22 +0100

    definierar publika och skyddade routes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,7 +10,11 @@ const Header = () => {
             <nav>
                 <ul>
                     <li><NavLink to="/">Startsida</NavLink></li>
-                    <li><NavLink to="/profile">Min sida</NavLink></li>
+                    <li><NavLink to="/posts">Blogg</NavLink></li>
+                    
+                    {user && (
+                        <li><NavLink to="/admin">Admin</NavLink></li>
+                    )}
                     <li>
                         {
                             !user ? <NavLink to="/login">Logga in</NavLink> : <button onClick={logout}>Logga ut</button>
--- src/routing.tsx
+++ src/routing.tsx
@@ -1,23 +1,34 @@
 import { createBrowserRouter } from "react-router-dom";
+import Layout from './components/Layout';
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import MyProfile from "./pages/MyProfile";
 import ProtectedRoute from "./components/ProtectedRoute";
+import PostListPage from "./pages/PostsListPage";
+import PostDetailsPage from "./pages/PostDetailsPage";
+import AdminPostsPage from './pages/AdminPostsPage';
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <Layout />,
         children: [
-            {
-                path: "/",
-                element: <HomePage />
+            { index: true, element: <HomePage /> },
+            { path: '/posts', element: <PostListPage /> },
+            { path: '/posts/:id', element: <PostDetailsPage /> },
+                {
+                    path: '/admin',
+                    element: (
+                        <ProtectedRoute>
+                            <AdminPostsPage />
+                        </ProtectedRoute>
+                    ),
             },
             {
                 path: "/profile",
                 element: (
                 <ProtectedRoute><MyProfile /></ProtectedRoute>
-                )
+                ),
             },
             {
                 path: "/login",
