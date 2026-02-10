0f2fd36b3d81759a499140a55f99edf33989a554
AmandaPersdotter <amanda.vivianne.p.l@hotmail.com>
2026-02-10 18:22:03 +0100

    skapa axios klient med auth interceptor

---
+++ src/api/postsApi.ts
@@ -0,0 +1,28 @@
+import { http } from "./http";
+import type { BlogPost, BlogPostCreate, BlogPostUpdate } from "../types/blog.types";
+
+export const postsApi = {
+    async list(): Promise<BlogPost[]> {
+        const res = await http.get<BlogPost[]>('/posts');
+        return res.data;
+    },
+
+    async get(id: string): Promise<BlogPost> {
+        const res = await http.get<BlogPost>(`/posts/${id}`);
+        return res.data;
+    },
+
+    async create(payload: BlogPostCreate): Promise<BlogPost> {
+        const res = await http.post<BlogPost>('/posts', payload);
+        return res.data;
+    },
+
+    async update(id: string, payload: BlogPostUpdate): Promise<BlodPost> {
+        const res = await http.put<BlogPost>(`/posts/${id}`, payload);
+        return res.data;
+    },
+
+    async remove(id: string): Promise<void> {
+        await http.delete(`/posts/${id}`);
+    },
+};
